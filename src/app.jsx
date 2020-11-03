668ee0e8836525f057eff3b9163580cb5b39b7f4
zaverden <zaverden@gmail.com>
2020-11-03 11:35:35 +0700

    final example

--- src/app.jsx
+++ src/app.jsx
@@ -2,21 +2,33 @@ import styled from "@emotion/styled";
 import { Fragment, useCallback, useEffect, useState } from "react";
 
 const Button = styled.button`
-  color: red;
+  color: blue;
+  margin-left: auto;
+  margin-right: auto;
 `;
-export function App() {
+export function Counter({ delta = 1 }) {
   const [count, setCount] = useState(0);
-  const increment = useCallback(() => setCount((c) => c + 10));
+  const increment = useCallback(() => setCount((c) => c + delta));
   useEffect(() => {
     if (count % 2 !== 0) {
       setCount((c) => c + 3);
     }
-  }, [count, increment]);
+  }, [count, setCount]);
   console.log(count);
   return (
     <Fragment>
-      <h2>Clicks: {count}</h2>
+      <h2>Clicks count: {count}</h2>
       <Button onClick={increment}>Click Me MORE!!!</Button>
+    </Fragment>
+  );
+}
+
+export function App() {
+  return (
+    <Fragment>
+      <Counter />
+      <hr />
+      <Counter delta={-1111} />
     </Fragment>
   );
 }
